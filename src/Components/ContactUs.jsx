2c6b4c4a0f0502d28ef65f9acbaec8374d4c8ac5
hanzlashah <108062644+hanzlashah@users.noreply.github.com>
2024-09-14 04:26:55 +0500

    form add

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
 
 function ContactUs() {
   return (
-    <div>
-      contact us 
-    </div>
-  )
+    <>
+      <div className="w-[90%] md:w-[75vw] mx-auto mb-[100px] md:mb-[4vw]">
+        <h2 className=" md:text-[2vw] lg:text-[2vw]  font-sans font-[700] pb-[20px] md:pb-[1.5vw] uppercase mb-[10px] md:mb-[3vw]">
+        Contact Us
+        </h2>
+        <div className="md:flex ">
+          <div className="md:w-[35vw]  px-[2vw] py-[1vw]">
+            <input
+              type="text"
+              className="w-[100%]  h-[50px] md:w-[31vw] md:h-[2.5vw] p-[20px] md:p-[.5vw] shadow-lg rounded-[.5vw] border-none outline-none mb-[20px] md:mb-[1vw]  bg-transparent"
+            />
+            <input
+              type="text"
+              className="w-[100%] h-[50px] md:w-[31vw] md:h-[2.5vw] p-[20px] md:p-[.5vw] shadow-lg rounded-[.5vw] border-none outline-none mb-[20px] md:mb-[1vw]  bg-transparent"
+            />
+            <input
+              type="text"
+              className="w-[100%] h-[50px] md:w-[31vw] md:h-[2.5vw] p-[20px] md:p-[.5vw] shadow-lg rounded-[.5vw] border-none outline-none mb-[20px] md:mb-[1vw]  bg-transparent"
+            />
+            <input
+              type="text"
+              className="w-[100%] h-[50px] md:w-[31vw] md:h-[2.5vw] p-[20px] md:p-[.5vw] shadow-lg rounded-[.5vw] border-none outline-none mb-[20px] md:mb-[1vw]  bg-transparent"
+            />
+            <input
+              type="text"
+              className="w-[100%] h-[50px] md:w-[31vw] md:h-[2.5vw] p-[20px] md:p-[.5vw] shadow-lg rounded-[.5vw] border-none outline-none mb-[20px] md:mb-[1vw]  bg-transparent"
+            />
+            <button className="py-[10px] md:py-[.5vw] rounded-[10px] font-[500] uppercase md:rounded-[.5vw] px-[20px] md:px-[2vw] hover:bg-transparent text-[#ffffff] hover:text-blue-400 bg-blue-400 border-transparent hover:border-blue-400 border text-[14px] md:text-[1vw]">
+              Send
+            </button>
+          </div>
+          <div>right</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default ContactUs
+export default ContactUs;
--- src/Components/Different_Services.jsx
+++ src/Components/Different_Services.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import s1 from "../../src/assets/s1.png";
-import s2 from "../../src/assets/s2.png"
-import s3 from "../../src/assets/s3.png"
+import s2 from "../../src/assets/s2.png";
+import s3 from "../../src/assets/s3.png";
 function Different_Services() {
   return (
     <div className="mt-[100px] md:mt-[3vw] mb-10 w-[80vw] md:mb-[2vw] text-center mx-auto">
@@ -28,10 +28,11 @@ function Different_Services() {
             className="h-[20%] w-[20%] md:w-[5vw] md:h-[5vw] mt-[10px] md:mt-[1vw]  text-[#555089]"
           />
           <h3 className="text-[15px] md:text-[1vw] font-[500]  uppercase">
-          Electrical
+            Electrical
           </h3>
           <p className="text-[12px] md:text-[1vw] font-[400] text-center mb-[20px] md:mb-[1vw] uppercase">
-          when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+            when looking at its layout. The point of using Lorem Ipsum is that
+            it has a more-or-less normal
           </p>
         </div>
         <div className="card w-[100%] h-[90%] hover:bg-[#ff8a1d] hover:text-[#ffff]   p-[1.5vw] rounded-[10px] border md:h-[20vw] md:w-[30vw] lg:w-[25vw] lg:h-[15vw] bg-white flex flex-col gap-[20px] md:gap-[1vw] justify-center items-center shadow">
@@ -40,10 +41,11 @@ function Different_Services() {
             className="h-[20%] w-[20%] md:w-[5vw] md:h-[5vw] mt-[10px] md:mt-[1vw] text-[#555089]"
           />
           <h3 className="text-[15px] md:text-[1vw] font-[500]  uppercase">
-          Plumbing
+            Plumbing
           </h3>
           <p className="text-[12px] md:text-[1vw] font-[400] text-center mb-[20px] md:mb-[1vw] uppercase">
-          when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+            when looking at its layout. The point of using Lorem Ipsum is that
+            it has a more-or-less normal
           </p>
         </div>
       </div>
