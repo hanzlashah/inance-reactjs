c263f921de0c54ca6e701e06b139edbb648a2adb
hanzlashah <108062644+hanzlashah@users.noreply.github.com>
2024-10-02 23:54:25 +0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "../../my-app/src/Components/Header";
-import Navigation from "../../my-app/src/Components/Navigation";
-import Hero from "../../my-app/src/Components/Hero";
-import AboutUs from "../../my-app/src/Components/AboutUs";
-import HomeServies from "../../my-app/src/Components/HomeServies";
-import Different_Services from "../../my-app/src/Components/Different_Services";
-import Testimonal from "../src/Components/Testimonal";
-import ContactUs from "../src/Components/ContactUs";
-import Footer from "../src/Components/Footer"
+import Header from "./Components/Header";
+import Navigation from "./Components/Navigation";
+import Hero from "./src/Components/Hero";
+import AboutUs from "./Components/AboutUs";
+import HomeServies from ".//Components/HomeServies";
+import Different_Services from "./Components/Different_Services";
+import Testimonal from "./src/Components/Testimonal";
+import ContactUs from "./src/Components/ContactUs";
+import Footer from " ./src/Components/Footer";
 function App() {
   return (
     <>
