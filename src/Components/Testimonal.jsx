2722028f79840c760d89a2251d50923587c187bc
hanzlashah <108062644+hanzlashah@users.noreply.github.com>
2024-10-02 23:46:53 +0500

    change

--- src/Components/Testimonal.jsx
+++ src/Components/Testimonal.jsx
@@ -13,7 +13,6 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import { fromJSON } from "postcss";
 
 function Testimonal() {
   return (
